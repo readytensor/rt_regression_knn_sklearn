a0fa4b449277ce6f6c63731e014fbb4cad2e457a
Abu Desai <abhyuday.desai@gmail.com>
2023-09-18 15:57:12 -0700

    update encoding in schema

--- src/prediction/predictor_model.py
+++ src/prediction/predictor_model.py
@@ -5,8 +5,8 @@ from typing import Optional
 import joblib
 import numpy as np
 import pandas as pd
-from sklearn.exceptions import NotFittedError
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.exceptions import NotFittedError
 
 warnings.filterwarnings("ignore")
 
